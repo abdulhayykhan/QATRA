716c43f2fd1a335a617085f9915fa8285cf978a3
Abdul Hayy Khan <ahkofficial.10@gmail.com>
2026-08-22 06:53:13 +0500

    chore: update Supabase dependency version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -119,7 +119,6 @@ dependencies {
   implementation(libs.firebase.ai)
   implementation(libs.firebase.messaging)
   implementation(libs.firebase.auth)
-  implementation(libs.supabase.core)
   implementation(libs.supabase.auth)
   implementation(libs.supabase.storage)
   implementation(libs.supabase.postgrest)
